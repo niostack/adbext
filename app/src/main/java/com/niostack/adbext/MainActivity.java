a75729f125d9c25c65a01ffcebb1da82ff365f7d
Your Name <you@example.com>
2023-11-04 22:12:51 +0800

    v1.1

--- app/src/main/java/com/niostack/adbext/MainActivity.java
+++ app/src/main/java/com/niostack/adbext/MainActivity.java
@@ -1,14 +1,25 @@
 package com.niostack.adbext;
 
+import android.Manifest;
+import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.app.AlarmManager;
 import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.content.res.Configuration;
+import android.os.Build;
 import android.os.Bundle;
+import android.telephony.SubscriptionInfo;
+import android.telephony.SubscriptionManager;
+import android.telephony.TelephonyManager;
 import android.webkit.WebView;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
 import java.lang.reflect.Field;
 import java.lang.reflect.Method;
@@ -25,13 +36,74 @@ import java.util.TimeZone;
  * adb shell am start -n com.niostack.adbext/.MainActivity --es language zh
  */
 public class MainActivity extends Activity {
+    private final int REQUEST_PHONE_STATE_PERMISSION = 123;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        // 检查是否已经获得了READ_PHONE_STATE权限
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.READ_PHONE_STATE)
+                != PackageManager.PERMISSION_GRANTED) {
+            // 如果没有获得权限，请求权限
+            ActivityCompat.requestPermissions(this,
+                    new String[]{Manifest.permission.READ_PHONE_STATE},
+                    REQUEST_PHONE_STATE_PERMISSION);
+        } else {
+            // 已经获得权限，可以执行需要该权限的操作
+            // 在此处调用获取MCC和MNC的代码
+            System.out.println("已经获得权限，可以执行需要该权限的操作");
+            setMCCMNC();
+        }
         setView();
     }
-    private void setView(){
+
+    private void setMCCMNC() {
+        String mccmnc = getMccMnc(this, 0);
+        TextView MCCMNC = findViewById(R.id.MCCMNC);
+        MCCMNC.setText(mccmnc);
+    }
+
+    // 处理权限请求的回调
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+        if (requestCode == REQUEST_PHONE_STATE_PERMISSION) {
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                // 用户授予了READ_PHONE_STATE权限，可以执行需要该权限的操作
+                // 在此处调用获取MCC和MNC的代码
+                System.out.println("用户授予了READ_PHONE_STATE权限");
+                setMCCMNC();
+            } else {
+                // 用户拒绝了权限请求，可以在这里处理拒绝的情况
+                System.out.println("用户拒绝了权限请求");
+            }
+        }
+    }
+
+    // 获取MCC和MNC
+    public String getMccMnc(Context context, int subscriptionId) {
+        TelephonyManager telephonyManager = (TelephonyManager) context.getSystemService(Context.TELEPHONY_SERVICE);
+
+        if (telephonyManager != null) {
+            if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.N) {
+                SubscriptionManager subscriptionManager = SubscriptionManager.from(context);
+                SubscriptionInfo subscriptionInfo = subscriptionManager.getActiveSubscriptionInfo(subscriptionId);
+
+                if (subscriptionInfo != null) {
+                    String mccMnc = subscriptionInfo.getMccString() + subscriptionInfo.getMncString();
+                    return mccMnc;
+                }
+            } else {
+                String mccMnc = telephonyManager.getSimOperator();
+                return mccMnc;
+            }
+        }
+
+        return null;
+    }
+    private void setView() {
         TextView et_current_timezone = findViewById(R.id.et_current_timezone);
         TextView et_current_language = findViewById(R.id.et_current_language);
         WebView tv_use_instructions_content = findViewById(R.id.tv_use_instructions_content);
@@ -41,6 +113,7 @@ public class MainActivity extends Activity {
         //获取当前手机系统时区设置
         String timeZone = TimeZone.getDefault().getID();
         et_current_timezone.setText(timeZone);
+
         //使用说明
 
         String use_instructions_content = getString(R.string.use_instructions_content);
--- readme.md
+++ readme.md
@@ -9,6 +9,7 @@
 <p><b>本工具提供了通过 adb 命令修改系统语言和时区的功能，使用方式如下：</b></p>
 <p><b>第一步：授权本工具的修改系统设置权限</b></p>
 <p><code>adb shell pm grant com.niostack.adbext android.permission.CHANGE_CONFIGURATION</code></p>
+<p><code>adb shell pm grant com.niostack.adbext android.permission.READ_PRIVILEGED_PHONE_STATE</code></p>
 <p><b>第二步：输入要修改的语言或时区</b></p>
 命令示例(修改语言为中文)：
 <p><code>adb shell am start -n com.niostack.adbext/.MainActivity --es language zh</code></p>
